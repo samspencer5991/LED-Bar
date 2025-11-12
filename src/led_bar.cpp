2d1123cdbaae3a014275cb168c8931a3a4e089a0
Sam Spencer <sam.spencer5991@gmail.com>
2025-11-12 13:39:53 +1100

    Update
    Changed handling for modes and LED mapping. All tested and working on CCW centre LED board

--- src/led_bar.cpp
+++ src/led_bar.cpp
@@ -1,6 +1,7 @@
 #include <led_bar.h>
 
-#define LED_CENTRE_DIMMING 2
+#define LED_CENTRE_DIMMING 4
+#define LED_START_DIMMING 4
 
 #define POT_WRAP_THRESHOLD 50
 
@@ -17,29 +18,241 @@ CHSV csvNeonRed = CHSV(2, 196, 255);
 int controlFocusToggle = -1;
 
 //------------------ Input Aquisition and Processing ----------------------
-void ledBar_Update(LEDBar *ledBar)
+void ledBar_Init(LEDBar* ledBar, uint16_t firstLedIndex, uint16_t numLeds, PotMode mode, PotWrapStyle wrap,
+	PotLedMode ledMode, uint8_t dataDirection, CHSV colour1, CHSV colour2)
 {
-	uint8_t ringPos = map(ledBar->value, 0, 1023, 0, ledBar->numLeds - 1);
-	uint16_t ledPos = 0;
-	uint16_t ledEnd = 0;
+	ledBar->ledArrayPtr = (CRGB*)malloc(sizeof(CRGB) * numLeds);
+	memset(ledBar->ledArrayPtr, 0, sizeof(CRGB) * numLeds);
+	ledBar->firstLedIndex = firstLedIndex;
+	ledBar->numLeds = numLeds;
+	ledBar->mode = mode;
+	ledBar->wrap = wrap;
+	ledBar->ledMode = ledMode;
+	ledBar->dataDirection = dataDirection;
+	ledBar->colours[0] = colour1;
+	ledBar->colours[1] = colour2;
+	ledBar->value = 0;
+	ledBar->interFade = 1; // Enable interpolation fading
+}
+void ledBar_Update(LEDBar* ledBar)
+{
+	uint16_t ringCount;				 // Number of LEDs in the ring for value feedback
+	uint16_t segmentSize;			 // Size of each segment in the pot value range
+	uint16_t ringPos;				 // Current position in the ring
+	CRGB interpolateLedColour;		 // Colour of the LED to be interpolated
+	uint16_t interpolatePos;		 // Position of the LED to be interpolated
+	uint8_t fadeAmount = 0;			 // Fade amount for interpolation
+	uint16_t positionInSegment;		 // Position within the current segment
+	uint8_t overflowMod;			 // Overflow from integer division of segments
+	static uint16_t valueUpdate = 0; // For debugging value changes
 
-	if(ledBar->dataDirection == 0)
+	// If the numLeds is even, do not light the bottom centre LED (reduce the count by 1)
+	ringCount = ledBar->numLeds;
+	if (ledBar->numLeds % 2 == 0)
+		ringCount -= 1;
+	segmentSize = 1024 / ringCount;
+	// Integer division gives segment index
+	ringPos = ledBar->value / segmentSize;
+	// Clamp to maximum position
+	if (ringPos >= ringCount)
+		ringPos = ringCount - 1;
+
+	
+
+	// The ledArrayPtr is a virtual mapping for the led bar.
+	// This allows for different physical arrangements of the LEDs
+	// The virtual mapping is always treated the same way regardless of physical layout
+	// This is then mapped to the actual leds[] array based on firstLedIndex and dataDirection
+	// The virtual mapping space always starts at index 0 to numLeds-1 in CW direction
+	// An LED in the bottom centre position is always considered the last LED in the ring (numLeds-1)
+
+	if (ledBar->mode == PotNormal)
+	{
+		if (ledBar->interFade)
+		{
+			positionInSegment = ledBar->value % segmentSize;
+			// Clamp the positionInSegment to the max ring position to avoid index overflow
+			// Get the overflow from the segment division
+			overflowMod = 1024 % ringCount;
+			if (ringPos == ringCount - 1 && ledBar->value >= 1024 - overflowMod)
+				positionInSegment = segmentSize;
+			if(ringPos == 0)
+				fadeAmount = map(positionInSegment, 0, segmentSize, (255/LED_START_DIMMING)-1, 255);
+			else
+				fadeAmount = map(positionInSegment, 0, segmentSize, 0, 255);
+		}
+		// Apply the ring colour mapping
+		if (ledBar->ledMode == PotLedFillGradient || ledBar->ledMode == PotLedDotGradient)
+		{
+			fill_gradient(ledBar->ledArrayPtr, 0, ledBar->colours[0],
+				ringCount-1, ledBar->colours[1], SHORTEST_HUES);
+		}
+		else if(ledBar->ledMode == PotLedFill || ledBar->ledMode == PotLedDot)
+		{
+			for (uint8_t j = 0; j < ringCount; j++)
+				ledBar->ledArrayPtr[j] = ledBar->colours[0];
+		}
+
+		// Fill modes
+		if (ledBar->ledMode == PotLedFillGradient || ledBar->ledMode == PotLedFill)
+		{
+			// Capture the previous LED position colour for interpolation fade
+			interpolatePos = ringPos;
+			interpolateLedColour = ledBar->ledArrayPtr[interpolatePos];
+			for (uint8_t j = ringPos+1; j < ringCount; j++)
+				ledBar->ledArrayPtr[j] = 0;
+
+			// Determine the faded brightness for interpolation
+			// This is done by dividing the input value range by the number of LEDs to map the delta to the next position
+			// Prevent the middle LED from being faded
+			if (ledBar->interFade)
+			{
+				fadeAmount = fadeAmount;
+				interpolateLedColour.nscale8(fadeAmount);
+				ledBar->ledArrayPtr[interpolatePos] = interpolateLedColour;
+			}
+		}
+
+		// Dot modes
+		else if(ledBar->ledMode == PotLedDotGradient || ledBar->ledMode == PotLedDot)
+		{
+			// Capture the current position LED colour
+			CRGB currentLedColour = ledBar->ledArrayPtr[ringPos];
+			// Turn off all LEDs
+			for (uint8_t j = 0; j < ledBar->numLeds; j++)
+				ledBar->ledArrayPtr[j] = 0;
+			// Light the current position LED
+			ledBar->ledArrayPtr[ringPos] = currentLedColour;
+		}
+	}
+	else if (ledBar->mode == PotCentred)
+	{
+		if (ledBar->interFade)
+		{
+			positionInSegment = ledBar->value % segmentSize;
+			// Clamp the positionInSegment to the max ring position to avoid index overflow
+			// Get the overflow from the segment division
+			overflowMod = 1024 % ringCount;
+			if (ringPos == ringCount - 1 && ledBar->value >= 1024 - overflowMod)
+				positionInSegment = segmentSize;
+			fadeAmount = map(positionInSegment, 0, segmentSize, 0, 255);
+		}
+		// Apply the ring colour mapping
+		if (ledBar->ledMode == PotLedFillGradient || ledBar->ledMode == PotLedDotGradient)
+		{
+			fill_gradient(ledBar->ledArrayPtr, 0, ledBar->colours[0],
+				ringCount - 1, ledBar->colours[1], SHORTEST_HUES);
+		}
+		else if(ledBar->ledMode == PotLedFill || ledBar->ledMode == PotLedDot)
+		{
+			for (uint8_t j = 0; j < ringCount; j++)
+				ledBar->ledArrayPtr[j] = ledBar->colours[0];
+		}
+
+		// Fill modes
+		if (ledBar->ledMode == PotLedFillGradient || ledBar->ledMode == PotLedFill)
+		{
+			// Capture the previous LED position colour for interpolation fade
+			interpolatePos = ringPos;
+			interpolateLedColour = ledBar->ledArrayPtr[interpolatePos];
+			// Current position in the lower half
+			if (ringPos < (ledBar->numLeds / 2 - 1))
+			{
+				// Turn off LEDs below current position
+				for (uint8_t j = 0; j < ringPos; j++)
+					ledBar->ledArrayPtr[j] = 0;
+
+				// Turn off LEDs above centre
+				for (uint8_t j = (ledBar->numLeds / 2); j < ledBar->numLeds; j++)
+					ledBar->ledArrayPtr[j] = 0;
+			}
+			// Current position in the upper half
+			else if (ringPos >= (ledBar->numLeds / 2 - 1))
+			{
+				// Turn off LEDs below centre
+				for (uint8_t j = 0; j < (ledBar->numLeds / 2 - 1); j++)
+					ledBar->ledArrayPtr[j] = 0;
+
+				// Turn off LEDs above current position
+				for (uint8_t j = ringPos + 1; j < ledBar->numLeds; j++)
+					ledBar->ledArrayPtr[j] = 0;
+			}
+			// Determine the faded brightness for interpolation
+			// This is done by dividing the input value range by the number of LEDs to map the delta to the next position
+			// Prevent the middle LED from being faded
+			if (ledBar->interFade && ringPos != (ledBar->numLeds / 2 - 1))
+			{
+				if (ringPos < (ledBar->numLeds / 2 - 1))
+				{
+					fadeAmount = 255 - fadeAmount;
+				}
+				else if (ringPos >= (ledBar->numLeds / 2 - 1))
+				{
+					fadeAmount = fadeAmount;
+				}
+				interpolateLedColour.nscale8(fadeAmount);
+				ledBar->ledArrayPtr[interpolatePos] = interpolateLedColour;
+			}
+		}
+		// Dot modes
+		else if(ledBar->ledMode == PotLedDotGradient || ledBar->ledMode == PotLedDot)
+		{
+			// Capture the centre and current position LED colours
+			CRGB centreLedColour = ledBar->ledArrayPtr[ledBar->numLeds / 2 - 1];
+			CRGB currentLedColour = ledBar->ledArrayPtr[ringPos];
+			// Turn off all LEDs
+			for (uint8_t j = 0; j < ledBar->numLeds; j++)
+				ledBar->ledArrayPtr[j] = 0;
+			// Light the centre and current position LEDS
+			// If the ringPos is not the centre LED, dim the centre
+			if (ringPos != (ledBar->numLeds / 2 - 1))
+			{
+				centreLedColour.nscale8(255 / LED_CENTRE_DIMMING);
+			}
+			ledBar->ledArrayPtr[ledBar->numLeds / 2 - 1] = centreLedColour;
+			ledBar->ledArrayPtr[ringPos] = currentLedColour;
+		}
+	}
+	if (ledBar->value != valueUpdate)
+	{
+		valueUpdate = ledBar->value;
+		char str[1024];
+		sprintf(str, "Ring Pos: %d,  Value: %d  Segment Size: %d,  PosInSeg: %d,  FadeAmt: %d\r\n", ringPos, ledBar->value, segmentSize, positionInSegment, fadeAmount);
+		Serial.print(str);
+	}
+
+	// Map the virtual ledArrayPtr to the actual leds[] array
+	for (uint16_t i = 0; i < ledBar->numLeds; i++)
+	{
+		uint16_t targetIndex = 0;
+		if (ledBar->dataDirection == 0)
+		{
+			targetIndex = ledBar->firstLedIndex + i;
+		}
+		else
+		{
+			targetIndex = ledBar->firstLedIndex + (ledBar->numLeds - 1) - i;
+		}
+		leds[targetIndex] = ledBar->ledArrayPtr[i];
+	}
+
+	/*
+	if (ledBar->dataDirection == 0)
 	{
 		ledPos = ringPos + ledBar->firstLedIndex;
-		ledEnd = ledBar->firstLedIndex + ledBar->numLeds-1;
+		ledEnd = ledBar->firstLedIndex + ledBar->numLeds - 1;
 	}
 	else
 	{
-		ledPos = ledBar->firstLedIndex + (ledBar->numLeds-1) - ringPos;
+		ledPos = ledBar->firstLedIndex + (ledBar->numLeds - 1) - ringPos;
 		ledEnd = ledBar->firstLedIndex;
 	}
-
 
 	if (ledBar->mode == PotNormal)
 	{
 		if (ledBar->ledMode == PotLedDot)
 		{
-			if(ledBar->dataDirection == 0)
+			if (ledBar->dataDirection == 0)
 			{
 				for (uint8_t j = ledBar->firstLedIndex; j <= ledEnd; j++)
 					leds[j] = 0;
@@ -55,13 +268,12 @@ void ledBar_Update(LEDBar *ledBar)
 		else if (ledBar->ledMode == PotLedDotGradient)
 		{
 			fill_gradient(leds, ledBar->firstLedIndex, ledBar->colours[0],
-							  ledEnd, ledBar->colours[1], SHORTEST_HUES);
+						  ledEnd, ledBar->colours[1], SHORTEST_HUES);
 			for (uint8_t j = ledPos + 1; j <= ledEnd; j++)
 				leds[j] = 0;
 
 			for (uint8_t j = ledBar->firstLedIndex; j < ledPos; j++)
 				leds[j] = 0;
-
 		}
 		else if (ledBar->ledMode == PotLedFill)
 		{
@@ -70,26 +282,24 @@ void ledBar_Update(LEDBar *ledBar)
 
 			for (uint8_t j = ledPos + 1; j <= ledEnd; j++)
 				leds[j] = 0;
-
 		}
 		else if (ledBar->ledMode == PotLedFillGradient)
 		{
-			
-			if(ledBar->dataDirection == 0)
+
+			if (ledBar->dataDirection == 0)
 			{
 				fill_gradient(leds, ledBar->firstLedIndex, ledBar->colours[0],
-					ledEnd, ledBar->colours[1], SHORTEST_HUES);
+							  ledEnd, ledBar->colours[1], SHORTEST_HUES);
 				for (uint8_t j = ledPos + 1; j <= ledEnd; j++)
 					leds[j] = 0;
 			}
 			else
 			{
 				fill_gradient(leds, ledBar->firstLedIndex, ledBar->colours[1],
-					ledBar->firstLedIndex + ledBar->numLeds-1, ledBar->colours[0], SHORTEST_HUES);
-				for(uint8_t j = ledBar->firstLedIndex; j <= ledPos-1; j++)
+							  ledBar->firstLedIndex + ledBar->numLeds - 1, ledBar->colours[0], SHORTEST_HUES);
+				for (uint8_t j = ledBar->firstLedIndex; j <= ledPos - 1; j++)
 					leds[j] = 0;
 			}
-
 		}
 	}
 	else if (ledBar->mode == PotCentred)
@@ -107,7 +317,7 @@ void ledBar_Update(LEDBar *ledBar)
 		else if (ledBar->ledMode == PotLedDotGradient)
 		{
 			fill_gradient(leds, ledBar->firstLedIndex, ledBar->colours[0],
-							  ledEnd, ledBar->colours[1], SHORTEST_HUES);
+						  ledEnd, ledBar->colours[1], SHORTEST_HUES);
 			for (uint8_t j = ledPos + 1; j <= ledEnd; j++)
 				leds[j] = 0;
 
@@ -130,24 +340,22 @@ void ledBar_Update(LEDBar *ledBar)
 			{
 				for (uint8_t j = ledPos; j <= ledBar->firstLedIndex + 10; j++)
 					leds[j] = ledBar->colours[0];
-
 			}
 			else if (ringPos > 10)
 			{
 				for (uint8_t j = ledBar->firstLedIndex + 10; j <= ledPos; j++)
 					leds[j] = ledBar->colours[0];
-
 			}
 		}
 		else if (ledBar->ledMode == PotLedFillGradient)
 		{
 			// CW direction
-			if(ledBar->dataDirection == 0)
+			if (ledBar->dataDirection == 0)
 			{
 				fill_gradient(leds, ledBar->firstLedIndex, ledBar->colours[0],
-								ledEnd, ledBar->colours[1], SHORTEST_HUES);
+							  ledEnd, ledBar->colours[1], SHORTEST_HUES);
 				// Current position in the lower half
-				if (ringPos < (ledBar->numLeds/2 - 1))
+				if (ringPos < (ledBar->numLeds / 2 - 1))
 				{
 					// Turn off LEDs below current position
 					for (uint8_t j = ledBar->firstLedIndex; j < ledPos; j++)
@@ -158,7 +366,7 @@ void ledBar_Update(LEDBar *ledBar)
 						leds[j] = 0;
 				}
 				// Current position in the upper half
-				else if (ringPos > (ledBar->numLeds/2 - 1))
+				else if (ringPos > (ledBar->numLeds / 2 - 1))
 				{
 					// Turn off LEDs below centre
 					for (uint8_t j = ledBar->firstLedIndex; j < ledBar->firstLedIndex + 10; j++)
@@ -173,23 +381,23 @@ void ledBar_Update(LEDBar *ledBar)
 			else
 			{
 				fill_gradient(leds, ledBar->firstLedIndex, ledBar->colours[1],
-					ledBar->firstLedIndex + ledBar->numLeds-1, ledBar->colours[0], SHORTEST_HUES);
+							  ledBar->firstLedIndex + ledBar->numLeds - 1, ledBar->colours[0], SHORTEST_HUES);
 				// Current position in the lower half
-				if (ringPos <= (ledBar->numLeds/2 - 1))
+				if (ringPos <= (ledBar->numLeds / 2 - 1))
 				{
 					// Turn off LEDs below current position
-					for (uint8_t j = (ledBar->firstLedIndex + ledBar->numLeds)-1; j > ledPos; j--)
+					for (uint8_t j = (ledBar->firstLedIndex + ledBar->numLeds) - 1; j > ledPos; j--)
 						leds[j] = 0;
 
 					// Turn off LEDs above centre
-					for (uint8_t j = ledBar->firstLedIndex; j <= ledBar->firstLedIndex + (ledBar->numLeds/2 - 1); j++)
+					for (uint8_t j = ledBar->firstLedIndex; j <= ledBar->firstLedIndex + (ledBar->numLeds / 2 - 1); j++)
 						leds[j] = 0;
 				}
 				// Current position in the upper half
-				else if (ringPos > (ledBar->numLeds/2 - 1))
+				else if (ringPos > (ledBar->numLeds / 2 - 1))
 				{
 					// Turn off LEDs below centre
-					for (uint8_t j = ledBar->firstLedIndex + ledBar->numLeds - 1; j > ledBar->firstLedIndex + (ledBar->numLeds/2); j--)
+					for (uint8_t j = ledBar->firstLedIndex + ledBar->numLeds - 1; j > ledBar->firstLedIndex + (ledBar->numLeds / 2); j--)
 						leds[j] = 0;
 
 					// Turn off LEDs above current position
@@ -199,5 +407,6 @@ void ledBar_Update(LEDBar *ledBar)
 			}
 		}
 	}
-	//FastLED.show();
+		*/
+		// FastLED.show();
 }
--- src/led_bar.h
+++ src/led_bar.h
@@ -9,41 +9,43 @@ extern CRGB leds[];
 
 typedef enum
 {
-    PotNormal,      // Pot is normalised to 0-1023
-    PotCentred,     // Pot is centred around 0 with +- 512 values either side
-    PotButtonOnly   // Pot is a button only
+	PotNormal,	  // Pot is normalised to 0-1023
+	PotCentred,	  // Pot is centred around 0 with +- 512 values either side
+	PotButtonOnly // Pot is a button only
 } PotMode;
 
 typedef enum
 {
-    PotLedFill,
-    PotLedDot,
-    PotLedFillGradient,
-    PotLedDotGradient
+	PotLedFill,
+	PotLedDot,
+	PotLedFillGradient,
+	PotLedDotGradient
 } PotLedMode;
 
 typedef enum
 {
-    PotNoWrap,
-    PotWrap
+	PotNoWrap,
+	PotWrap
 } PotWrapStyle;
 
 typedef struct
 {
-    PotMode mode;                                       // Control UI mode
-    PotWrapStyle wrap;                                  // Control wrap style 
-    PotLedMode ledMode;                                 // Control LED UI mode
-    CHSV colours[2];                                    // Ring display colours
-    uint16_t value;                                     // Current value
-	 uint16_t numLeds;						// Number of LEDs in the bar
-	 uint16_t firstLedIndex;				// Position of the first LED in the bar
-	 uint8_t dataDirection;					// 0 = bottom to top or CW, 1 = top to bottom or CCW	
-	 uint8_t interFade;						// Interpolation fade on or off		
+	PotMode mode;			// Control UI mode
+	PotWrapStyle wrap;		// Control wrap style
+	PotLedMode ledMode;		// Control LED UI mode
+	CHSV colours[2];		// Ring display colours
+	uint16_t value;			// Current value
+	uint16_t numLeds;		// Number of LEDs in the bar
+	CRGB *ledArrayPtr;		// Array for the LED virtual mapping strip
+	uint16_t firstLedIndex; // Position of the first LED in the bar
+	uint8_t dataDirection;	// 0 = bottom to top or CW, 1 = top to bottom or CCW
+	uint8_t interFade;		// Interpolation fade on or off
 } LEDBar;
 
 // Public function prototypes
-void ledBar_Update(LEDBar* ledBar);
-
+void ledBar_Init(LEDBar *ledBar, uint16_t firstLedIndex, uint16_t numLeds, PotMode mode, PotWrapStyle wrap,
+				 PotLedMode ledMode, uint8_t dataDirection, CHSV colour1, CHSV colour2);
+void ledBar_Update(LEDBar *ledBar);
 
 // HSV Colours
 // Neon
